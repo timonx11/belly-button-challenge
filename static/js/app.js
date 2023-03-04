55141300ec729bf48ed7d0fc9f8ad42ea8a22c2d
timonx11 <TimN@winslow.com.au>
2023-03-04 22:22:43 +1100

    published git html and read data api

---
+++ static/js/app.js
@@ -0,0 +1,6 @@
+const bbdata = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+// Fetch the JSON data and console log it
+d3.json(bbdata).then(function(data) {
+  console.log(data);
+});
---
+++ static/js/bonus.js
@@ -0,0 +1,6 @@
+const bonus = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+// Fetch the JSON data and console log it
+d3.json(bonus).then(function(data) {
+  console.log(data);
+});
