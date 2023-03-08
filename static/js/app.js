6aafd69a20fa9e1051822e9ca784d6045be12cd3
timonx11 <TimN@winslow.com.au>
2023-03-08 23:49:44 +1100

    final commits

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,153 @@
+// Get the Belly Button endpoint
 const bbdata = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 // Fetch the JSON data and console log it
 d3.json(bbdata).then(function(data) {
   console.log(data);
 });
+
+// Display and initialize the default plot
+function init(){
+    //assigning variable to the dropdown menu
+    let dropdownMenu = d3.select("#selDataset");
+
+    d3.json(bbdata).then((data) => {
+                // Set a variable for the sample.json names object
+                let names = data.names;
+        
+                // Add  samples.names.id to dropdown menu
+                data.names.forEach((id) => {
+
+                    // Log the value of id for each iteration of the loop
+                    console.log(id);
+        
+                    dropdownMenu.append("option")
+                    .text(id)
+                    .property("value",id);
+                });
+
+                // Set the first sample from the list
+                let sample_data = names[0];
+
+                // Log the value of sample_one
+                console.log(sample_data);
+
+                // Display initial \ default data plots
+                barbubbleCharts(sample_data);
+                Metadata(sample_data);
+    });
+}
+
+// Function for displaying and adding bar and bubble info \ chart
+function barbubbleCharts(sample) {
+
+    // Use D3 to retrieve all of the data
+    d3.json(bbdata).then((data) => {
+
+        // Retrieve all samples data
+        let sample_data = data.samples;
+
+        // Filter value of the samples data
+        let value = sample_data.filter(result => result.id === sample);
+
+        // Get the first index from the array
+        let valueData = value[0];
+
+        // Get the otu_ids, lables, and sample value data
+        let otu_ids = valueData.otu_ids;
+        let otu_labels = valueData.otu_labels;
+        let sample_values = valueData.sample_values;
+
+        // Log the data to the console
+        console.log(otu_ids,otu_labels,sample_values);
+
+        // slicing top ten data to display in descending order for the bar chart
+        let yticks = otu_ids.slice(0,10).map(id => `OTU ${id}`);
+        let xticks = sample_values.slice(0,10).reverse();
+        let labels = otu_labels.slice(0,10);
+        
+        // Set up the plotly trace for the bar chart
+        let trace = {
+            x: xticks,
+            y: yticks,
+            text: labels,
+            type: "bar",
+            orientation: "h"
+        };
+
+        // Use d3.Plotly to plot the bar chart
+        Plotly.newPlot("bar", [trace])
+    
+
+    // Set up the plotly trace for bubble chart
+        let trace1 = {
+            x: otu_ids,
+            y: sample_values,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+            },
+            text: otu_labels
+        };
+
+    // Set up the layout
+        let layout = {
+            hovermode: "closest",
+            xaxis: {title: "OTU ID"},
+        };
+
+    // Use d3.Plotly to plot the bubble chart
+        Plotly.newPlot("bubble", [trace1], layout)
+    });
+};
+
+// Function for displaying and adding metadata \ demographic info
+function Metadata(sample) {
+
+    // Use D3 to retrieve all of the data
+    d3.json(bbdata).then((data) => {
+
+        // Retrieve all metadata data
+        let metadata = data.metadata;
+
+        // Filter value of the samples data
+        let value = metadata.filter(result => result.id == sample);
+
+        // Log the data to the console
+        console.log(value)
+
+        // Get the first index from the array
+        let valueData = value[0];
+
+         // Clear out metadata panel on changed
+         d3.select("#sample-metadata").html("");
+
+        // Use Object.entries to add each key/value pair to the metadata panel
+        Object.entries(valueData).forEach(([key,value]) => {
+
+            // Log the individual key/value pairs to be added in to the metadata panel
+            console.log(key,value);
+            //add the value pairs into the metadata panel
+            d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`);
+        });
+
+    });
+
+};
+
+// Function that updates dashboard when data is changed
+function optionChanged(value) { 
+
+    // Log the new value
+    console.log(value); 
+
+    // Call all functions to update charts \ data as per user selection
+    barbubbleCharts(value);
+    Metadata(value);
+    GaugeChart(value);
+};
+
+// Calling the init function
+init();
--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,6 +1,101 @@
+// Get the Belly Button bonus endpoint
 const bonus = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 // Fetch the JSON data and console log it
 d3.json(bonus).then(function(data) {
   console.log(data);
 });
+
+// Display and initialize the default plot for bonus questions
+function init(){
+  //assigning variable to the dropdown menu
+  let dropdownMenu = d3.select("#selDataset");
+
+  d3.json(bbdata).then((data) => {
+              // Set a variable for the sample names
+              let names = data.names;
+      
+              // Add  samples to dropdown menu
+              data.names.forEach((id) => {
+      
+                  // Log the value of id for each iteration of the loop
+                  console.log(id);
+      
+                  dropdownMenu.append("bonus")
+                  .text(id)
+                  .property("value",id);
+              });
+
+              // Set the first sample from the list
+              let sample_data = names[0];
+
+              // Log the value of sample_one
+              console.log(sample_data);
+
+              // Display initial \ default data plots
+              GaugeChart(sample_data);
+  });
+}
+
+// Function that builds the gauge chart
+function GaugeChart(sample) {
+
+  // Use D3 to retrieve all of the data
+  d3.json(bonus).then((data) => {
+
+      // Retrieve all metadata
+      let metadata = data.metadata;
+
+      /// Filter value of the samples data
+      let value = metadata.filter(result => result.id == sample);
+
+      // Log the data to the console
+      console.log(value)
+
+      // Get the first index from the array
+      let valueData = value[0];
+
+      // Use Object.values to get the frequency vale to be added to the gauge chart
+      let wfreq = Object.values(valueData)[6];
+      
+      // Set up the plotly trace for the gauge chart
+      let trace2 = {
+          domain: {x: [0, 1], y: [0, 1]},
+          value: wfreq,
+          type: "indicator",
+          mode: "gauge+number",
+          title: {
+              text: "<b>Belly Button Washing Frequency</b><br>Scrubs per Week",
+              font: {color: "black", size: 16}
+          },
+          gauge: {
+              axis: {range: [0,10], tickwidth: 1,},
+              BarP: {color: "green"},
+              steps: [
+                  {range: [0, 1], color: "rgba(255, 255, 255, 0)"},
+                  {range: [1, 2], color: "rgba(222, 222, 202, 0.5)"},
+                  {range: [2, 3], color: "rgba(200, 209, 145, 0.5)"},
+                  {range: [3, 4], color:  "rgba(200, 219, 95, 0.5)"},
+                  {range: [4, 5], color:  "rgba(174, 215, 68, 0.5)"},
+                  {range: [5, 6], color: "rgba(160, 212, 42, 0.5)"},
+                  {range: [6, 7], color: "rgba(132, 198, 35 , 0.5)"},
+                  {range: [7, 8], color:  "rgba(120, 174, 22, 0.5)"},
+                  {range: [8, 9], color: "rgba(60, 163, 10, 0.5)"},
+                  {range: [9, 10], color: "rgba(24, 157, 7, 0.5)"},
+              ]
+          } 
+      };
+
+      // Set up the Layout
+      let layout = {
+          width: 500, 
+          height: 500,
+          margin: {t: 0, b:0}
+      };
+
+      // Use d3.Plotly to plot the gauge chart
+      Plotly.newPlot("gauge", [trace2], layout)
+  });
+};
+
+init();
